bcc18cddfecf10f83731433f83972e8ce013b2e4
MUKUND209076 <71828007+MUKUND209076@users.noreply.github.com>
2023-09-05 14:31:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+window.sr = ScrollReveal();
+sr.reveal('.content');
+var v1=true
+let handleMargin=()=>{
+    if(v1===true){
+        // CODE FOR EXPAND MARGIN
+        // console.log("INSIDE TRUE")
+        document.querySelectorAll('.top-text').forEach(item => {
+            item.style.setProperty('margin-top','315px')
+        });
+
+
+        v1=false
+    }
+    else{
+        // CODE FOR REMOVE MARGIN
+        // console.log("INSIDE FALSE")
+        setTimeout(()=>{
+        document.querySelectorAll('.top-text').forEach(item => {
+            item.style.setProperty('margin-top','0')
+        });
+    },150)
+        v1=true
+    }
+}
